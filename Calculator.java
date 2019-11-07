a4dfb8cf0f188bbbe2392c5e82724636747862be
Boris Budini <boris@budini.xyz>
2019-11-07 08:43:16 +0100

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    double num1;
+    double num2;
+    String operator;
+    double results;
+
+    int num3;
+
+    void Addition() {
+            results = num1+num2;
+    }
+    
+    void Substraction() {
+            results = num1-num2;
+    }
+    void Multiplication() {
+            results = num1*num2;
+    }
+    void Division() {
+            results = num1/num2;
+    }
+    
+    void Module() {
+        results = num1%num2;
+    }
+
+    
+}
